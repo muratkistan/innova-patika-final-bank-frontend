922be79df0d89c2276dbe31682b775cfe261674d
muratkistan <muratkistan@gmail.com>
2022-02-27 02:59:56 +0300

    Added ListCreditsComponents page

---
+++ src/components/ListCreditsComponent.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import CreditService from "../services/CreditService";
+const ListCreditsComponent = () => {
+  const [credits, setCredits] = useState([]);
+
+  useEffect(() => {
+    getCredits();
+  }, []);
+
+  const getCredits = (response) => {
+    CreditService.getAllCredits()
+      .then((response) => {
+        setCredits(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="tepe">
+      <div className="container mt-8">
+        <h2 className="text-center"> ALL CREDITS </h2>
+        <table className="table table-bordered table-striped  ">
+          <thead>
+            <th> Credit Id </th>
+            <th> Identity Number </th>
+            <th> Limit</th>
+            <th> Status</th>
+          </thead>
+          <tbody>
+            {credits.map((credit) => (
+              <tr key={credit.id}>
+                <td> {credit.id} </td>
+                <td> {credit.identityNumber} </td>
+                <td> {credit.creditLimit} </td>
+                {credit.status ? (
+                  <td style={{ color: "green" }}>
+                    <b>ACCEPTED</b>{" "}
+                  </td>
+                ) : (
+                  <td style={{ color: "red" }}>
+                    <b>DENIED</b>{" "}
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListCreditsComponent;
